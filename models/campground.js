339a3973fc1e4487a2670ea711ffbb6ecd165809
Kiana <kianaperst85@gmail.com>
2023-12-05 14:10:08 -0500

    Adding Cluster Map

--- models/campground.js
+++ models/campground.js
@@ -11,6 +11,8 @@ ImageSchema.virtual('thumbnail').get(function() {
     return this.url.replace('/upload', '/upload/w_200');
 })
 
+const opts = {toJSON: {virtuals: true}};
+
 const CampgroundSchema = new Schema ({
     title: String,
     images: [ImageSchema],
@@ -36,6 +38,10 @@ const CampgroundSchema = new Schema ({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }]
+}, opts)
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
+    return `<p><b>${this.title}</b></p><a href="/campgrounds/${this._id}">View</a>`;
 })
 
 CampgroundSchema.post('findOneAndDelete', async function(doc){
